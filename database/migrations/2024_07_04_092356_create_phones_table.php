5575848e2c61f8f009e886f404245e809b7c2d5d
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-07-04 13:50:24 +0330

    finish country and phones

--- database/migrations/2024_07_04_092356_create_phones_table.php
+++ database/migrations/2024_07_04_092356_create_phones_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,17 @@ return new class extends Migration
     {
         Schema::create('phones', function (Blueprint $table) {
             $table->id();
+            $table->string("number")->unique();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignId('country_id')
+                ->constrained('countries')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
