58d19395e1a71d9b50b8680a7de75a7e700dea63
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-07-07 12:19:00 +0330

    add category to project

---
+++ database/factories/Manager/CategoryFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Manager;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Manager\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
