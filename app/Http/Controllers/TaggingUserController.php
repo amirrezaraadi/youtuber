7b8debe099854af83638a91375eef92f7b2e10bd
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-07-07 21:15:14 +0330

    add Tagging users to project

---
+++ app/Http/Controllers/TaggingUserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaggingUserRequest;
+use App\Http\Requests\UpdateTaggingUserRequest;
+use App\Models\Manager\TaggingUser;
+
+class TaggingUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaggingUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TaggingUser $taggingUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TaggingUser $taggingUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaggingUserRequest $request, TaggingUser $taggingUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TaggingUser $taggingUser)
+    {
+        //
+    }
+}
